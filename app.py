3503247e2e86f5cf3199beea583e26b834107f4e
Maciej Prostak <maciekprostak@gmail.com>
2019-02-02 12:52:41 +0100

    Background schedular for getting scrape data

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask, Response
 from flask_cors import CORS
 from scrape import scrape
-#from flask_restful import Resource, Api, reqparse, abort
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 CORS(app)
 
-sampleList = scrape()
+# TO DO
+# Parse json here and add success message
+# Only change main object if new update
+# Dont change main object if it's not working
+newsFeed = ''
+
+def updateNewsFeed():
+    global newsFeed
+    feed = scrape()
+    newsFeed = feed
 
 @app.route('/')
 def printList():
     return Response(
-            response=sampleList,
+            response=newsFeed,
             status=200,
             mimetype='application/json')
+
+scheduler = BackgroundScheduler(timezone="UTC")
+scheduler.add_job(updateNewsFeed, 'interval', seconds=15)
+scheduler.start()
+
+if __name__ == '__main__':
+    app.run()
