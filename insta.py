ab7b7940db11a51ba609b52bfc2cfc37b7da6457
h-okon <hubrr.oko@gmail.com>
2019-02-04 18:20:19 +0100

    cleaning

--- insta.py
+++ insta.py
@@ -1,25 +1,3 @@
-"""
-from instalooter.looters import ProfileLooter
-looter = ProfileLooter("umcs_lublin", dump_json=True)
-def downloadInstagram():
-	looter.download('media/', media_count=5)
-
-def links(media, looter):
-    if media.get('__typename') == "GraphSidecar":
-        media = looter.get_post_info(media['shortcode'])
-        nodes = [e['node'] for e in media['edge_sidecar_to_children']['edges']]
-        return [n.get('video_url') or n.get('display_url') for n in nodes]
-    elif media['is_video']:
-        media = looter.get_post_info(media['shortcode'])
-        return [media['video_url']]
-    else:
-        return [media['display_url']]
-
-if __name__ == "__main__":
-	downloadInstagram()
-"""
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import json
 import re
 import requests
